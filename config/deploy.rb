d7c80d6b10611586978feccde5cb432f37470c11
Rodrigo Sol <rodrigosol@gmail.com>
2012-01-19 15:44:26 -0200

    up

---
+++ config/deploy.rb
@@ -0,0 +1,36 @@
+
+set :application, "Khronos"
+
+# If you aren't deploying to /u/apps/#{application} on the target
+# servers (which is the default), you can specify the actual location
+# via the :deploy_to variable:
+set :deploy_to, "/app/khronos"
+ 
+# If you aren't using Subversion to manage your source code, specify
+# your SCM below:
+default_run_options[:pty] = true
+
+set :scm, :git
+set :repository, "git@github.com:rodrigosol/casamento.git"
+set :branch, "master"
+set :deploy_via, :remote_cache
+
+set :user, 'raro'
+set :ssh_options, { :forward_agent => true }
+ 
+role :app, "casamentonaroca.com.br"
+role :web, "casamentonaroca.com.br"
+role :db,  "casamentonaroca.com.br", :primary => true
+ 
+namespace :deploy do
+  desc "Restarting mod_rails with restart.txt"
+  task :restart, :roles => :app, :except => { :no_release => true } do
+    run "touch #{current_path}/tmp/restart.txt"
+  end
+ 
+  [:start, :stop].each do |t|
+    desc "#{t} task is a no-op with mod_rails"
+    task t, :roles => :app do ; end
+  end
+end
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Casamento::Application.config.secret_token = '4ff961b41c633b4956c1ccd2119330a9e6a00c71ad564ed0777c0b59bd7b8c5c18a250029e266972515118d6cd8c92b09f87322624a6867afef464d906864e7d'
