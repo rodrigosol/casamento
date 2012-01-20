6a400cb30763a19d68f0ffcc24e48b1f239cbd70
Rodrigo Sol <rodrigosol@gmail.com>
2012-01-19 22:36:12 -0200

    ops

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,10 +1,10 @@
 
-set :application, "Khronos"
+set :application, "Casamento"
 
 # If you aren't deploying to /u/apps/#{application} on the target
 # servers (which is the default), you can specify the actual location
 # via the :deploy_to variable:
-set :deploy_to, "/app/khronos"
+set :deploy_to, "/app/casamento"
  
 # If you aren't using Subversion to manage your source code, specify
 # your SCM below:
@@ -18,9 +18,9 @@ set :deploy_via, :remote_cache
 set :user, 'raro'
 set :ssh_options, { :forward_agent => true }
  
-role :app, "casamentonaroca.com.br"
-role :web, "casamentonaroca.com.br"
-role :db,  "casamentonaroca.com.br", :primary => true
+role :app, "184.106.107.116"
+role :web, "184.106.107.116"
+role :db,  "184.106.107.116", :primary => true
  
 namespace :deploy do
   desc "Restarting mod_rails with restart.txt"
