960ebeaa91d114ab1c55a0aff99a337ddb6c5f61
Rodrigo Sol <rodrigosol@gmail.com>
2012-01-19 23:30:59 -0200

    prd

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -33,4 +33,5 @@ Casamento::Application.configure do
   config.assets.compress = false
 
   # Expands the lines which load the assets
-  config.assets.debug = trueend
+  config.assets.debug = true
+end
